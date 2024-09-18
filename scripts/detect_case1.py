fdeae125087c4528ec186c47e38b0f5b4b9c0d24
Cac1231 <152652434+Cac1231@users.noreply.github.com>
2024-09-18 15:19:16 -0700

    Update detect_case1.py

--- scripts/detect_case1.py
+++ scripts/detect_case1.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from methods_composite_null_case1 import betting_experiment
+from methods_case1 import betting_experiment
 from fractions import Fraction
 
 def load_data(file_path, data_type):
