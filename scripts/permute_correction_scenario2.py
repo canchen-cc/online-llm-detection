a016f5152f00e725f25b2c892cb188d7ed21a632
Cac1231 <152652434+Cac1231@users.noreply.github.com>
2024-10-09 18:01:13 -0700

    Update and rename permute_correction_case2.py to permute_correction_scenario2.py

--- scripts/permute_correction_scenario2.py
+++ scripts/permute_correction_scenario2.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from methods_case2 import betting_experiment, seq_perm_test_experiment
+from methods_scenario2 import betting_experiment, seq_perm_test_experiment
 from fractions import Fraction
 
 def load_data(file_path, data_type):
