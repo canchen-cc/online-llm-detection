93b4e11d43f4f29e39d853ab04a621f1f16c7879
Cac1231 <152652434+Cac1231@users.noreply.github.com>
2024-10-10 17:27:10 -0700

    Update permute_no_correction_scenario2.py

--- scripts/permute_no_correction_scenario2.py
+++ scripts/permute_no_correction_scenario2.py
@@ -20,6 +20,11 @@ def load_data(file_path, data_type):
         print(f"Error: Could not read {file_path}")
         return None
 
+def show_score_function(filepath):
+    filename = os.path.basename(filepath)
+    part = filename.split('.')[-2]
+    return part
+
 def process_files(file1, type1, file2, type2, file3, type3, k, alphas, iters, shift_time):
     data1 = load_data(file1, type1)
     data2 = load_data(file2, type2)
@@ -40,11 +45,12 @@ def process_files(file1, type1, file2, type2, file3, type3, k, alphas, iters, sh
     perm_k_tau, perm_k_tpr = seq_perm_test_experiment(y1, y2, alphas, iters, k=k, bonferroni=False)
     _, perm_k_fdr = seq_perm_test_experiment(z1, z2, alphas, iters, k=k, bonferroni=False)
     
+    part=show_score_function(file1)
     
     return {
-        "k_value": k,
-        "seq1_dataset": file1,
-        "seq2_dataset": file2,
+        "batch_size": k,
+        "score_function": part,
+        "time_budget": min_len,
         "rejection_time": np.ceil(np.mean(perm_k_tau, axis=0)), #rejection time
         "power": np.mean(perm_k_tpr, axis=0),#true=1/false=0
         "fpr": np.mean(perm_k_fdr, axis=0) #type-1 error
